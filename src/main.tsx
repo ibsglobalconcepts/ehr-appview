11b4d6fcbd352f4dbe3e00d2a11e1b8eaf434101
ibsglobalconcepts <ibsglobalconcepts@gmail.com>
2025-11-28 09:52:41 +0100

    frontend install

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { store } from "./app/store.js";
-import './styles.css'
+import './styles/index.css'   // ✅ Correct path
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
