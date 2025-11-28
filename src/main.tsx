d66913739e140def22545fee22198e91d2b7cad2
ibsglobalconcepts <ibsglobalconcepts@gmail.com>
2025-11-28 09:39:27 +0100

    frontend install

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { store } from './store'
+import { store } from "./app/store.js";
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
